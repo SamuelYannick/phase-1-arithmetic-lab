e61aacf48cc20367e156aedf2b422142452d6dc4
SamuelOchieng <o.samuelyannick@gmail.com>
2024-11-25 23:37:41 +0300

    arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let num1 = 2, num2 = 31, multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 5, num4 = 29, mod = num4 % num3;
+
+let max = Math.max(3, 7, 14, 20);
